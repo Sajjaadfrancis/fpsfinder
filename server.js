74beac23035e3fd2cf4f5f112e59d4cb35d2e6d5
Sajjaad Francis <sajjaadfrancis@gmail.com>
2020-09-16 18:02:43 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const { use } = require("./routes/profile");
+
+// load env
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+
+// profile routes
+app.use("/api/v1/profile", require("./routes/profile"));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
